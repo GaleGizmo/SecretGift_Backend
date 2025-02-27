668022f2b9a9053b1fe129ca94266307dc5fef5b
GaleGizmo <juanmiguelabelleira@gmail.com>
2025-02-27 11:09:48 +0100

    restaurado campo password en user

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-   
+    password: { type: String, required: false },
     logged: { type: Boolean, default: false },
     owned_games: [{ type: mongoose.Types.ObjectId, ref: "room" }],
     played_games: [{ type: mongoose.Types.ObjectId, ref: "room" }],
