e17e5e9e2adce62612e48c113385da37a968907b
GaleGizmo <juanmiguelabelleira@gmail.com>
2025-03-11 12:00:24 +0100

    rules deja de tener id

--- src/api/room/room.model.js
+++ src/api/room/room.model.js
@@ -1,46 +1,48 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const PlayerSchema = new Schema({
-    player_name: { type: String, required: true },
-    email: { type: String, required: true },
-    linked_to: { type: String, required: true },
-    player_code: { type: String, required: true }
-  });
-  
-  const RuleSchema = new Schema({
-    player_1: { type: String, required: true },
-    player_2: { type: String, required: true }
-  });
-const RoomSchema = new Schema({
-   
+  player_name: { type: String, required: true },
+  email: { type: String, required: true },
+  linked_to: { type: String, required: true },
+  player_code: { type: String, required: true },
+});
+
+// const RuleSchema = new Schema({
+//   player_1: { type: String, required: true },
+//   player_2: { type: String, required: true },
+// });
+const RoomSchema = new Schema(
+  {
     game_name: { type: String, required: true },
     game_date: { type: Date, required: true },
-    owner_id: { type: mongoose.Types.ObjectId, ref: 'user', required: true },
+    owner_id: { type: mongoose.Types.ObjectId, ref: "user", required: true },
     min_cost: { type: Number, required: false },
     max_cost: { type: Number, required: false },
-    status: { type: String, required: true, enum: ['active', 'finished'] },
+    status: { type: String, required: true, enum: ["active", "finished"] },
     game_code: { type: String },
     players: {
-        type: [PlayerSchema],
-        validate: {
-          validator: function (players) {
-            return players.length >= 3; // Asegura que haya al menos 3 jugadores
-          },
-          message: "Necesitas al menos 3 jugadores."
-        }
+      type: [PlayerSchema],
+      validate: {
+        validator: function (players) {
+          return players.length >= 3; // Asegura que haya al menos 3 jugadores
+        },
+        message: "Necesitas al menos 3 jugadores.",
       },
-      rules: {
-        type: [RuleSchema],
-        default: [] // Hace que el campo sea opcional
-      }
+    },
+    rules: [
+      {
+        player_1: { type: String, required: true },
+        player_2: { type: String, required: true },
+      },
+    ],
   },
   {
     timestamps: true,
-    collection: 'room',
-  });
+    collection: "room",
+  }
+);
 
-  
-  const Room = mongoose.model('room', RoomSchema);
-  
+const Room = mongoose.model("room", RoomSchema);
+
 export default Room;
