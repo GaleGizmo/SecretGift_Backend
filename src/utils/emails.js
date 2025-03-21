93889ef86a96b682e914999ede3c50b45ba5c50a
GaleGizmo <juanmiguelabelleira@gmail.com>
2025-03-21 13:20:37 +0100

    sendEmails comprueba info.accepted.length)

--- src/utils/emails.js
+++ src/utils/emails.js
@@ -20,7 +20,11 @@ async function sendEmails(destinatario, asunto, mensaje) {
     });
 
     console.log("Correo enviado: " + info.response);
-    return { status: "success", response: info.response };
+    if (info.accepted.length > 0) {
+      return { status: "success", response: info.response };
+    } else {
+      return { status: "error", error: "Correo no aceptado por el servidor del destinatario" };
+    }
   } catch (error) {
     console.error("Error al enviar el correo: ", error);
     return { status: "error", error: error.message };
